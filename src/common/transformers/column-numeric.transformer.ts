f041242842721cd6a6efda7b2f5ca8d2f140fc85
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2023-01-06 05:12:46 +0200

    Another fix

---
+++ src/common/transformers/column-numeric.transformer.ts
@@ -0,0 +1,8 @@
+export class ColumnNumericTransformer {
+    to(data: number): number {
+        return data;
+    }
+    from(data: string): any {
+        return parseFloat(data);
+    }
+}
--- src/core/banks/entities/bank.entity.ts
+++ src/core/banks/entities/bank.entity.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { BaseEntitySchema } from 'src/common/schemas/base-entity.schema';
+import { ColumnNumericTransformer } from 'src/common/transformers/column-numeric.transformer';
 import { TransactionEntity } from 'src/core/transactions/entities/transaction.entity';
 import { Column, Entity, OneToMany } from 'typeorm';
 
@@ -10,7 +11,13 @@ export class BankEntity extends BaseEntitySchema {
     name!: string;
 
     @ApiProperty({ default: 0.0 })
-    @Column({ type: 'numeric', precision: 7, scale: 2, default: 0.0 })
+    @Column({
+        type: 'numeric',
+        precision: 7,
+        scale: 2,
+        transformer: new ColumnNumericTransformer(),
+        default: 0.0,
+    })
     balance?: number;
 
     @OneToMany(() => TransactionEntity, (transaction) => transaction.bank)
