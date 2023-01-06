6bdd5e8f694a27899fc0310c820cc9b597788696
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2023-01-06 11:23:24 +0200

    Added Api Logger

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { BanksModule } from './banks/banks.module';
 import { CategoriesModule } from './categories/categories.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { StatisticsModule } from './statistics/statistics.module';
+import { AppLoggerMiddleware } from './app-logger.middleware';
 
 @Module({
     imports: [BanksModule, CategoriesModule, TransactionsModule, StatisticsModule],
 })
-export class CoreModule {}
+export class CoreModule {
+    configure(consumer: MiddlewareConsumer): void {
+        consumer.apply(AppLoggerMiddleware).forRoutes('*');
+    }
+}
